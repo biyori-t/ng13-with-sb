27fdffd1b7cfbc9788ea04c7d3f9f3a19df8a414
biyori-t <takuro.toji@gmail.com>
2022-06-19 15:43:16 +0900

    Page のCSF を 3.0 に更新

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,4 +1,4 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { moduleMetadata, Meta, StoryObj } from '@storybook/angular';
 import { within, userEvent } from '@storybook/testing-library';
 import { CommonModule } from '@angular/common';
 
@@ -21,16 +21,15 @@ export default {
   ],
 } as Meta;
 
-const Template: Story<Page> = (args: Page) => ({
-  props: args,
-});
+export const LoggedOut: StoryObj<Page> = {
+  args: {},
+};
 
-export const LoggedOut = Template.bind({});
-
-// More on interaction testing: https://storybook.js.org/docs/angular/writing-tests/interaction-testing
-export const LoggedIn = Template.bind({});
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+export const LoggedIn: StoryObj<Page> = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+    await userEvent.click(loginButton);
+  },
 };
